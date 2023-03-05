3504018f02db33b8d4d4dd7ecf7bf8436109ef5e
unknown <hilmi09mallick@gmail.com>
2023-03-05 16:28:36 +0200

    test one

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express"); // Used to set up a server
 const cors = require("cors"); // Used to prevent errors when working locally
+const path = require("path");
+const userRoute = require("./routes/userRoute");
 
 const app = express(); // Initialize express as an app variable
 app.set("port", process.env.PORT || 6969); // Set the port
@@ -9,6 +11,8 @@ app.use(cors()); // Dont let local development give errors
 app.get("/", (req, res) => {
     res.json({ msg: "Welcome" });
 });
+
+app.use("/users", userRoute);
 
 app.listen(app.get("port"), () => {
     console.log(`Listening for calls on port ${app.get("port")}`);
